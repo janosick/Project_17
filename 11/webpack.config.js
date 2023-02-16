46cd70457b5ad385addc9041ad350fbbe3e85fe3
janosick <arcadego337@gmail.com>
2023-02-16 12:05:43 +0100

    17_lesson_11

---
+++ 11/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+    context: path.join(__dirname, 'src'),
+    entry: {
+        index: './index',
+        shop: './shop'
+    },
+
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Application title 01',
+            hash: true,
+            template: './template.html'
+        })
+    ]
+};
