e8c31927ab513b178dbf29535389256962b54cbd
janosick <arcadego337@gmail.com>
2023-02-16 12:02:32 +0100

    17_lesson_06

---
+++ 6/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+
+    entry: {
+        index: './Home',
+        shop: './Shop',
+        profile: './Profile'
+    },
+
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
