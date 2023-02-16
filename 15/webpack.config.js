0ac0cc213ce0dc10e48e86d147d2b7c272be58d9
janosick <arcadego337@gmail.com>
2023-02-16 12:08:30 +0100

    17_lesson_15

---
+++ 15/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
+module.exports = {
+    context: path.join(__dirname, 'src'),
+    entry: {
+        index: './index'
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'less-loader']
+                })
+            }
+        ]
+    },
+
+    plugins: [
+        new ExtractTextPlugin({
+            filename: 'styles.css',
+            allChunks: true
+        })
+    ]
+};
